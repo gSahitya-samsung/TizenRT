b7e68023df12d55dd2076d1cda34e8d5e3cea5ba
Gaurav Sahitya <g.sahitya@samsung.com>
2023-10-05 12:55:50 +0530

    apps/examples/: Add Write Validation

--- apps/examples/fs_write_test/fs_write_main.c
+++ apps/examples/fs_write_test/fs_write_main.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * Copyright 2016 Samsung Electronics All Rights Reserved.
+ * Copyright 2023 Samsung Electronics All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
  *
  ****************************************************************************/
 /****************************************************************************
- * examples/hello/hello_main.c
+ * examples/fs_write_test/fs_write_main.c
  *
  *   Copyright (C) 2008, 2011-2012 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
@@ -67,26 +67,90 @@
 #include <sys/stat.h>
 #include <string.h>
 
-#define ROOT                            "/mnt/"
-#define SAMPLE_FILE                     ROOT "sample_file"
-#define TEST_DIR_SINGLE                 ROOT "single"
-#define TEST_FILE_SINGLE                ROOT "single/test_file"
-#define TEST_DIR_MULTIPLE               ROOT "multiple"
-#define TEST_FILE_MULTIPLE              ROOT "multiple/test_file_"
-#define TEST_FILE_NAME_LEN_MAX          42
-#define TEST_ITR                        10
-#define TIMER_DEVICE 			"/dev/timer0"
-#define FS_PART_SIZE			512
-
-
-// #define OK 0
-#define ERR -1
+#define ROOT "/mnt/"
+#define SAMPLE_FILE ROOT "sample_file"
+#define TEST_DIR_SINGLE ROOT "single"
+#define TEST_FILE_SINGLE ROOT "single/test_file"
+#define TEST_DIR_MULTIPLE ROOT "multiple"
+#define TEST_FILE_MULTIPLE ROOT "multiple/test_file_"
+#define TEST_FILE_NAME_LEN_MAX 42
+#define TEST_ITR 10
+#define TIMER_DEVICE "/dev/timer0"
+#define FS_PART_SIZE 512
 
 int frt_fd = -1;
+
+// #define OK 0
 
 /****************************************************************************
  * fs_write_main
  ****************************************************************************/
+
+
+void validate(char *buf, char *fileName, int nChar)
+{
+	#ifdef CONFIG_EXAMPLES_FS_WRITE_VALIDATE
+
+	int fd = open(fileName, O_RDONLY);
+
+	if(fd < 0) {
+		printf("Unable to open file = %s, error = %s\n", fileName, strerror(errno));
+		printf("Write Verification Failed\n");
+		// return -errno;
+		// goto errHandler;
+	}
+
+	// int fileLen =  
+
+	char *tempBuf = (char *) malloc(nChar + 1);
+	int ret = lseek(fd, -nChar, SEEK_END);
+
+	// printf("Lseek = %d\n", ret);
+
+	if(ret < 0) {
+		printf("Unable to lseek file = %s, error = %s\n", fileName, strerror(errno));
+		goto errHandler;
+	}
+
+
+	ret = read(fd, tempBuf, nChar);
+
+	// printf("%s\n", tempBuf);
+	// printf("%s\n", buf);
+
+	if (ret != nChar) {
+		printf("%d\n", nChar);
+		printf("Unable to read file = %s, error = %s, ret = %d\n",fileName, strerror(errno), ret);
+		goto errHandler;
+	}
+
+	for (int i = 0; i < nChar; i++) {
+		if (buf[i] != tempBuf[i]) {
+			printf("Write Verification Failed\n");
+			printf("Expected to write: %.*s\n", nChar, buf);
+			printf("Written: %s\n", tempBuf);
+			close(fd);
+			free(tempBuf);
+			return;
+		}
+	}
+
+	printf("Write Verification Success\n");
+	printf("Expected to write: %.*s\n", nChar, buf);
+	printf("Written: %s\n", tempBuf);
+
+	free(tempBuf);
+	close(fd);
+	return OK;
+
+	errHandler:
+		close(fd);
+		free(tempBuf);
+		printf("Write Verification Failed");
+		return;
+	#endif
+
+}
 
 int init_sample_file(int buffer_size)
 {
@@ -95,7 +159,7 @@ int init_sample_file(int buffer_size)
 
 	if (fd < 0) {
 		printf("Unable to open sample file: %s, fd = %d\n", SAMPLE_FILE, fd);
-		printf("%s\n",strerror(errno));
+		printf("%s\n", strerror(errno));
 		return -ENOMEM;
 	}
 
@@ -108,7 +172,7 @@ int init_sample_file(int buffer_size)
 		if (wrtLen != bufLen) {
 			printf("Unable to write to sample file, fd = %d, write length = %d", fd, wrtLen);
 			close(fd);
-			return ERR;
+			return -errno;
 		}
 	}
 
@@ -123,78 +187,90 @@ int create_PPE(const char *dir_path)
 
 	if (ret < 0) {
 		printf("Unable to create test directory = %s\n", dir_path);
-		printf("%d\n",errno);
-		printf("%s\n",strerror(errno));
-		if(errno != 17){
+		printf("%d\n", errno);
+		printf("%s\n", strerror(errno));
+		if (errno != 17) {
 			goto errHandler;
 		}
 	}
 
-	int fd = open(SAMPLE_FILE, O_RDONLY);
+	int fd_sample = open(SAMPLE_FILE, O_RDONLY);
 
-	if (fd < 0) {
+	if (fd_sample < 0) {
 		printf("Unable to open sample file for reading\n");
-		printf("%s\n",strerror(errno));
+		printf("%s\n", strerror(errno));
 		printf(SAMPLE_FILE);
 		goto errHandler;
 	}
 
-	// char buf[64 * 1024 + 1];
-	char *buf = (char *) malloc(64 * 1024 + 1);
-	ret = read(fd, buf, 64 * 1024);
+	// char buf[32 + 1];
+	char *buf = (char *) malloc(2 * 1024 + 1);
+	// ret = read(fd, buf, 64 * 1024);
 
-	if (ret != 64 *1024) {
-		printf("Unable to read from Sample File\n, ret = %d", ret);
-		close(fd);
-		free(buf);
-		goto errHandler;
-	}
+	// if (ret != 64 * 1024) {
+	// 	printf("Unable to read from Sample File\n, ret = %d", ret);
+	// 	close(fd);
+	// 	free(buf);
+	// 	goto errHandler;
+	// }
 
-	close(fd);
+	// close(fd);
 
 	for (int i = 0; i < ((FS_PART_SIZE - 128) / 64); i++) {
 		printf("Writing Garbage file #%d\n", i);
 
-		fd = open(TEST_FILE_SINGLE, O_CREAT | O_WRONLY);
+		int fd = open(TEST_FILE_SINGLE, O_CREAT | O_WRONLY);
 
 		if (fd < 0) {
 			printf("Failed to open garbage file for creation\n");
+			free(buf);
 			goto errHandler;
 		}
 
-		ret = write(fd, buf, 64 * 1024);
+		for (int j = 0; j < 32; j++) {
+			ret = read(fd_sample, buf, 2 * 1024);
 
-		if (ret != 64 * 1024) {
-			printf("Unable to write to garbage file\n");
-			goto errHandler;
+			if (ret != 2 * 1024) {
+				printf("Unable to read from Sample File\n, ret = %d", ret);
+				close(fd_sample);
+				free(buf);
+				goto errHandler;
+			}
+
+			ret = write(fd, buf, 2 * 1024);
+
+			if (ret != 2 * 1024) {
+				printf("Unable to write to garbage file\n");
+				close(fd);
+				free(buf);
+				goto errHandler;
+			}
+
 		}
+		
+		lseek(fd_sample, 0, SEEK_SET);
+
+		// ret = write(fd, buf, 64 * 1024);
+
+		// if (ret != 64 * 1024) {
+		// 	printf("Unable to write to garbage file\n");
+		// 	goto errHandler;
+		// }
 
 		close(fd);
 		unlink(TEST_FILE_SINGLE);
 	}
 
 	free(buf);
+	close(fd_sample);
 
 	printf("PPE Exits\n");
 	return OK;
 
-	errHandler:
-		printf("RRE Exits\n");
-		return -EIO;
-
+errHandler:
+	printf("RRE Exits\n");
+	return -EIO;
 }
-
-// int customWrite(int fd, char *buf, char *fileName, int size)
-// {
-// 	int ret = write(fd, buf, size);
-// 	if (ret != size) {
-// 		printf("Unable to write to file %s, fd = %d\n", fileName, ret);
-// 		close(fd);
-// 		return ERR;
-// 	}
-// 	return ret;
-// }
-
 
 int create_max_file(char *fileName, long int size)
 {
@@ -207,10 +283,10 @@ int create_max_file(char *fileName, long int size)
 		return -ENOMEM;
 	}
 
-	while (size>=strlen(buf)) {
+	while (size >= strlen(buf)) {
 		int ret = write(fd, buf, strlen(buf));
 
-		if(ret != strlen(buf)) {
+		if (ret != strlen(buf)) {
 			printf("Unable to write to file %s, fd = %d\n", fileName, ret);
 			close(fd);
 			return -ENOMEM;
@@ -233,147 +309,97 @@ int create_max_file(char *fileName, long int size)
 	close(fd);
 
 	return OK;
-
-}
-
-int big_f_small_o()
-{
-	char buf[] = "This is data in the temporary buffer. This will be written to the file for testing Filesystems with TizenRT RTOS.\n";
-	char fileName[TEST_FILE_NAME_LEN_MAX];
-
-	int ret = create_PPE(TEST_DIR_SINGLE);
-
-	if (ret != OK) {
-		printf("Unable to create PPE\n");
-		return ERR;
-	}
-
-	ret = mkdir(TEST_DIR_MULTIPLE, 0777);
-	snprintf(fileName, TEST_FILE_NAME_LEN_MAX, "%s%d", TEST_FILE_MULTIPLE, 0);
-
-	ret = create_max_file(fileName, 200 * 1024);
-
-	if (ret == OK) {
-		printf("File of size 200 KB created\n");
-	}
-
-	long int pos = 0;
-	unsigned long int test_time = 0;
-
-	int arr[] = {20, 50, 75, 100, 150, 200, -1};
-	int n = sizeof(arr) / sizeof(arr[0]);
-
-	for (int i = 0;  i < n; i++) {
-		struct timeval start, end;
-		gettimeofday(&start, NULL);
-
-		for (int j = 0; j<50; j++) {
-			int fd = open(fileName, O_WRONLY);
-
-			if (fd < 0) {
-				printf("Unable to open file %s\n", fileName);
-				return ERR;
-			}
-
-			lseek(fd, pos, SEEK_SET);
-
-			ret = write(fd, buf, strlen(buf));
-
-
-			if (ret != strlen(buf)) {
-				printf("Failed to write at pos = %ld, j = %d\n", pos, i);
-				close(fd);
-				return ERR;
-			}
-
-			close(fd);
-		}
-
-		gettimeofday(&end, NULL);
-
-		unsigned long int total_time = (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec;
-
-		printf("#############################################################################################\n");
-		printf("Average time taken to overwrite %d bytes at position %ld is: %llu\n", strlen(buf), pos, total_time / 50);
-		printf("#############################################################################################\n");
-		pos = arr[i] * 1024;
-	}
-
-	return OK;
 }
 
 void test()
 {
 
-	char writeBuffer[240+1];
+	char writeBuffer[240 + 1];
 
 	int ret = create_PPE(TEST_DIR_SINGLE);
 
 	if (ret != OK) {
-            printf("Unable to create PPE\n");
-            goto fileCreateError;
-    }
+		printf("Unable to create PPE\n");
+		goto fileCreateError;
+	}
 
 	printf("PPE Complete\n");
 
 	int fd = open(SAMPLE_FILE, O_RDONLY);
 
 	if (fd < 0) {
-        printf("Failed to open sample file for reading\n");
-        goto fileCreateError;
-    }
+		printf("Failed to open sample file for reading\n");
+		goto fileCreateError;
+	}
 
 	ret = read(fd, writeBuffer, 240);
 
 	if (ret != 240) {
 		printf("Unable to read from sample file\n");
-		printf("%s\n",strerror(errno));
+		printf("%s\n", strerror(errno));
 	}
 
 	close(fd);
+
+	char multiFileName[TEST_FILE_NAME_LEN_MAX];
+
+	for (int i = 0; i < 4; i++) {
+		snprintf(multiFileName, TEST_FILE_NAME_LEN_MAX, "%s_%d", TEST_FILE_SINGLE, i);
+		ret = create_max_file(multiFileName, 64 * 1024);
+		if (ret != OK) {
+			printf("Unable to create dummy file\n");
+			printf("%s\n", strerror(errno));
+			goto fileCreateError;
+			//return -errno;
+		}
+	}
 
 	ret = mkdir(TEST_DIR_MULTIPLE, S_IRWXG | S_IRWXO | S_IRWXU);
 
 	if (ret < 0) {
 		printf("Unable to create test directory = %s\n", TEST_DIR_MULTIPLE);
-		printf("%d\n",errno);
-		printf("%s\n",strerror(errno));
-		if(errno != 17){
+		printf("%d\n", errno);
+		printf("%s\n", strerror(errno));
+		if (errno != 17) {
 			goto fileCreateError;
 		}
 	}
 
 	for (int bufSize = 60; bufSize <= 240; bufSize *= 2) {
 		int totalTime = 0;
-		//memset(writeBuffer, 'a', bufSize * sizeof(char));
 
 		for (int itr = 0; itr < 20; itr++) {
-
-			//time_t start, end;
 
 			struct timeval start, end;
 			gettimeofday(&start, NULL);
 
-			//time(&start);
+			int nFiles = (bufSize == 240) ? 8 : 10;
 
-			for(int file = 1; file < 10; file ++){
+			for (int file = 1; file <= nFiles; file++) {
 
-				// int fd;
 				char fileName[30];
 
 				snprintf(fileName, 30, "%s%d.txt", TEST_FILE_MULTIPLE, file);
 
 				fd = open(fileName, O_WRONLY | O_CREAT);
 
-				if(fd<0){
+				if (fd < 0) {
 					printf("Unable to create file, ret = %d\n", errno);
 					goto fileCreateError;
 				}
 
-				write(fd, writeBuffer, bufSize);
-				close(fd);
+				int ret = write(fd, writeBuffer, bufSize);
 
-				for(int i=1; i<200; i++){
+				if (ret != bufSize) {
+					printf("Unable to write to file = %s, error = %s\n", fileName, strerror(errno));
+					close(fd);
+					goto fileCreateError;
+				}
+
+				close(fd);
+				validate(writeBuffer, fileName, bufSize);
+
+				for (int i = 1; i < 200; i++) {
 					fd = open(fileName, O_WRONLY);
 
 					if (fd < 0) {
@@ -381,38 +407,32 @@ void test()
 						goto fileCreateError;
 					}
 
-
 					lseek(fd, 0, SEEK_END);
 					write(fd, writeBuffer, bufSize);
 					close(fd);
+					validate(writeBuffer, fileName, bufSize);
 				}
-
 			}
 
-			//time(&end);
 			gettimeofday(&end, NULL);
 
-			for(int file = 0; file < 10; file++){
+			for (int file = 0; file < 10; file++) {
 				char fileName[30];
 				snprintf(fileName, 30, "%s%d.txt", TEST_FILE_MULTIPLE, file);
 				unlink(fileName);
 			}
 
-			//double itrTime = difftime(end,start);
-
 			int itrTime = (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec;
 
 			printf("Time taken: %lu microseconds\n", itrTime);
-			totalTime+=itrTime;
-
+			totalTime += itrTime;
 		}
 
 		printf("Total time taken for buffer of size: %d bytes: %lu microseconds\n", bufSize, totalTime);
-
 	}
 
-	fileCreateError:
-		return ;
+fileCreateError:
+	return;
 }
 
 // #define CONFIG_BUILD_KERNEL 1
@@ -432,18 +452,6 @@ int fs_write_main(int argc, char *argv[])
 
 	test();
 
-	// ret = big_f_small_o();
-
-	// if (ret != OK) {
-	// 	printf("Big_F_Small_O test failed, ret = %d\n", ret);
-	// 	goto errHandler;
-	// }
-
-	// printf("FS Performance Test Completed successfully\n");
-	// ret = OK;
-
-	errHandler:
-		return ret;
-
-	// return 0;
+errHandler:
+	return ret;
 }
