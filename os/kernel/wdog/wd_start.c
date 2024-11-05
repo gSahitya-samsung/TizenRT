1b24e2e52f4addd3726f712f5dc872cbce8a138b
eunwoo.nam <eunwoo.nam@samsung.com>
2024-11-05 19:04:48 +0900

    wdog/wd_timer_nohz: Move collecting wd missing tick to pm_wakehandler
    There is an issue where after waking up from UART RX, even if pm_timedsuspend is called,
    it immediately enters enter_board_sleep again.
    
    since, The missed ticks while the board is asleep will be compensated for after waking up.
    At this time, the wd timer is corrected in the first tick ISR that comes after wakeup.
    
    but UART RX ISR is comming first than tick ISR.
    So even if call pm_timedsuspend whick is using wd timer, it will expired in first tick ISR after wakeup.
    
    The issue flow likes below:
    WAKE HANDLER -> UART RX ISR -> UART READ THREAD -> call pm_timedsuspend -> TICK ISR (Collect missing tick and expire)
    
    Therefore, Move collecting wd missing tick to pm_wakehandler.
    but The wd timer is collected in pm_wakehandler and the timer expiration is performed in the first ISR after wakeup.
    This is to avoid delaying the response of the UART or other HW IRQ ISR due to a timer expiration operation.
    
    The fixed flow:
    WAKE HANDLER (Collect missing tick) -> UART RX ISR -> UART READ THREAD -> call pm_timedsuspend -> TICK ISR (expire)
    
    Signed-off-by: eunwoo.nam <eunwoo.nam@samsung.com>

--- os/kernel/wdog/wd_start.c
+++ os/kernel/wdog/wd_start.c
@@ -454,30 +454,6 @@ void wd_timer(void)
 {
 	/* Check if there are any active watchdogs to process */
 
-#ifdef CONFIG_SCHED_TICKSUPPRESS
-	FAR struct wdog_s *wdog;
-	int decr;
-
-	while (g_wd_ticks > 0 && g_wdactivelist.head) {
-		/* Get the watchdog at the head of the list */
-
-		wdog = (FAR struct wdog_s *)g_wdactivelist.head;
-
-		/* Decrement the lag for this watchdog. */
-
-		decr = MIN(wdog->lag, g_wd_ticks);
-
-		/* There are.  Decrement the lag counter */
-
-		wdog->lag -= decr;
-		g_wd_ticks -= decr;
-
-		/* Check if the watchdog at the head of the list is ready to run */
-
-		wd_expiration();
-	}
-#endif
-
 	if (g_wdactivelist.head) {
 		/* There are.  Decrement the lag counter */
 
@@ -494,6 +470,21 @@ void wd_timer(void)
 #ifdef CONFIG_SCHED_TICKSUPPRESS
 void wd_timer_nohz(clock_t ticks)
 {
-	g_wd_ticks += ticks;
+	FAR struct wdog_s *wdog;
+	int decr;
+
+	for (wdog = g_wdactivelist.head; ticks > 0 && wdog; wdog = wdog->next) {
+
+		/* Decrement the lag for this watchdog. */
+
+		decr = MIN(wdog->lag, ticks);
+
+		/* There are.  Decrement the lag counter */
+
+		wdog->lag -= decr;
+		ticks -= decr;
+
+		/* Expires when the next wd_timer is called.*/
+	}
 }
 #endif
--- os/pm/pm_wakehandler.c
+++ os/pm/pm_wakehandler.c
@@ -62,7 +62,21 @@ void pm_wakehandler(clock_t missing_tick, pm_wakeup_reason_code_t wakeup_src)
 	pmllvdbg("missing_tick: %llu\n", missing_tick);
 #ifdef CONFIG_PM_TICKSUPPRESS
 	if (missing_tick > 0) {
+		/* Correcting for missed system ticks in sleep. */
 		clock_timer_nohz(missing_tick);
+
+		/* Corrects for missed wd timer ticks during sleep.
+		 * But to compensate for the operation time of the HW IRQ handler in wakeup,
+		 * the wd_timer does not expire during here.
+		 *
+		 * The expiration action of wd timer is performed in the first tick isr after
+		 * wakeup.
+		 *
+		 *     WAKE HANDLER -> HW IRQ ISR -> THREAD -> TICK ISR
+		 *           |              |          |           |
+		 *           +--------------+----------+-----------+
+		 *     (corrects tick)                       (expire timer)
+		 */
 		wd_timer_nohz(missing_tick);
 	}
 #endif
